cf650a1867d7b345045e7a5ecb11c2cb10cf79b1
haiyee99 <37543660+haiyee99@users.noreply.github.com>
2022-09-27 10:40:21 +0700

    FIX: fix logger duplicate output

--- calx/logger.py
+++ calx/logger.py
@@ -2,12 +2,16 @@ import logging
 
 
 def create_logger(name: str = "calx", level=logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.hasHandlers():
+        return logger
+
     formatter = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
 
-    logger = logging.getLogger(name)
     logger.addHandler(console_handler)
     logger.setLevel(level)
 
